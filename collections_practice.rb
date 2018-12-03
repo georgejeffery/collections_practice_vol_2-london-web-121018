dea4dc15b23ddd2e653c594836479f1ab400ceae
georgejeffery <georgejeffery@gmail.com>
2018-12-03 14:32:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
-# your code goes here
+def begins_with_r(array)
+  num = 0
+  array.each do |i|
+    name = i[0]
+    if name == "r"
+      num += 1
+    end
+  end
+  if num == (array.length)
+      true
+  else
+      false
+  end
+end
